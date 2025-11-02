555956f7ed0bb07daf6c103f05152164ba5161ac
Lucas28reis <crreacicomp@gmail.com>
2025-11-02 08:44:29 -0300

    -add arquitecture and creating llama3

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # TeacherIA
 It is an application to assist students and researchers in their study journey with the summary technique.
+
+```
+TeacherIA
+├─ docker-compose.yml
+├─ README.md
+├─ requirements.txt
+├─ src
+│  ├─ backend
+│  │  ├─ controller
+│  │  ├─ inputconfig.py
+│  │  ├─ models
+│  │  │  └─ llama3.py
+│  │  ├─ prompt
+│  │  │  ├─ examples.md
+│  │  │  ├─ instructions.md
+│  │  │  └─ persona_tutor.md
+│  │  └─ view
+│  ├─ book
+│  └─ frontend
+└─ test
+
+```
---
+++ src/backend/llm/tutor_service.py
@@ -0,0 +1,29 @@
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
+
+messages = [
+    {
+        "role": "system", 
+        "content": "Você é um professor de matemática rígido que é bem detalhista e sempre exige o melhor dos alunos"
+    }, 
+    
+    {
+        "role": "user",
+        "content": "Quanto é 55 * 100"
+    }
+]
+
+
+inputs = tokenizer.apply_chat_template(
+	messages,
+	add_generation_prompt=True,
+	tokenize=True,
+	return_dict=True,
+	return_tensors="pt",
+).to(model.device)
+
+outputs = model.generate(**inputs, max_new_tokens=40)
+print(tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
